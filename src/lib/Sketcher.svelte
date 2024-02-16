ecc65e342032f370475712af611d03857417f0db
Brad Phelan <brad@moduleworks.com>
2024-02-16 21:22:42 +0100

    Use immer for state control

--- src/lib/Sketcher.svelte
+++ src/lib/Sketcher.svelte
@@ -4,86 +4,109 @@
   import Polyline from "./Polyline.svelte";
   import type { Point, Curve, Curves } from "../types";
   import { douglasPeucker } from "../utils";
+  import type { Action } from "svelte/action";
+  import { produce } from "immer";
 
   type Mode = "freehand" | "lines" | "edit";
+  type Action =
+    | "mousedown"
+    | "mousemove"
+    | "mouseup"
+    | "mouseout"
+    | "mousedblclick";
 
-  let isDrawing = false;
   let mode: Mode = "freehand"; // Default mode
-  let currentCurve: Curve  = [];
-  export let curves: Curves = [];
-  let tempLineStart: Point | null = null;
-  let closeCurves = true;
+  let closeCurves: boolean = true;
 
   type PointIndex = {
     curve: number;
     point: number;
-  } 
-  
-  let editedPoint : PointIndex|undefined = undefined;
+  };
 
-  function push_point(point: Point) {
-    currentCurve.push(point);
-    currentCurve = currentCurve;
-    curves = curves;
+  let editedPoint: PointIndex | undefined = undefined;
+
+  type State = {
+    curves: Curves;
+    drawing: boolean;
+    tempLineStart: Point | null;
+  };
+
+  let state = {
+    curves: [] as Curves,
+    drawing: false,
+    tempLineStart: null as Point | null,
+  };
+
+  export let curves = state.curves;
+
+  function push_point(point: Point, state: State): State {
+    return produce(state, (draft) => {
+      if (draft.curves.length == 0) draft.curves.push([]);
+      draft.curves[draft.curves.length - 1].push(point);
+    });
   }
 
-  function new_curve() {
-    currentCurve = [];
-    curves.push(currentCurve);
-    curves = curves;
+  function pop_point(state: State): State {
+    return produce(state, (draft) => {
+      if (draft.curves.length > 0) draft.curves[draft.curves.length - 1].pop();
+    });
+  }
+
+  function new_curve(state: State) {
+    return produce(state, (draft) => {
+      draft.curves.push([]);
+    });
+  }
+
+  function pop_curve(state: State) {
+    return produce(state, (draft) => {
+      draft.curves.pop();
+    });
+  }
+
+  function startDrawing(state: State) {
+    return produce(state, (draft) => {
+      draft.drawing = true;
+    });
+  }
+
+  function stopDrawing(state: State) {
+    return produce(state, (draft) => {
+      draft.drawing = false;
+    });
   }
 
   function changeMode(newMode: Mode) {
     mode = newMode;
-    if (mode === "freehand") {
-      // Clear the current curve and start a new one
-      currentCurve = [];
-    }
+    state = new_curve(state);
   }
 
-  function clear() {
-    curves = [];
-    currentCurve = [];
-    tempLineStart = null;
-  }
-
-  function smoothCurves(tolerance: number) {
-    curves = curves.map((curve) => douglasPeucker(curve, tolerance));
-  }
-
-  function handleMouseDown(event: MouseEvent) {
-    isDrawing = true;
-    const newPoint: Point = [event.offsetX, event.offsetY];
-    if (mode === "freehand") {
-      // Start a new curve in freehand mode
-      new_curve();
-      push_point(newPoint);
-    } else if (mode === "lines") {
-      // Add a point to the current curve or start a new one
-      if (currentCurve.length === 0) {
-        // Start a new curve
-        new_curve();
-        push_point(newPoint);
-      } else {
-        // Add a point to the current curve
-        push_point(newPoint);
+  function finish_curve(state: State) {
+    return produce(state, (draft) => {
+      if (draft.curves.length == 0) return;
+      if (closeCurves) {
+        let currentCurve = draft.curves[draft.curves.length - 1];
+        currentCurve.push(currentCurve[0]);
       }
-      tempLineStart = newPoint; // Start the temporary line
-    } else if (mode === "edit") {
-      // Find the closest point to the mouse and drag it
-      editedPoint = GetEditedPoint(event);
-    }
+    });
   }
 
-  function GetEditedPoint(event: MouseEvent) : PointIndex | undefined {
+  function clear(state: State) {
+    return produce(state, (draft) => {
+      draft.curves = [];
+      draft.tempLineStart = null;
+    });
+  }
+
+  function GetEditedPoint(event: MouseEvent): PointIndex | undefined {
     let minDistance = Infinity;
     let editedCurve = undefined;
     let editedPoint = undefined;
-    for (let i = 0; i < curves.length; i++) {
-      for (let j = 0; j < curves[i].length; j++) {
+    for (let i = 0; i < state.curves.length; i++) {
+      for (let j = 0; j < state.curves[i].length; j++) {
         const distance = Math.sqrt(
-          (curves[i][j][0] - event.offsetX) ** 2 +
-            (curves[i][j][1] - event.offsetY) ** 2
+          (state.curves[i][j][0] - event.offsetX) ** 2 +
+            (state.curves[i][j][1] - event.offsetY) ** 2
         );
         if (distance < minDistance) {
           minDistance = distance;
@@ -92,64 +115,92 @@
         }
       }
     }
-    if(editedCurve&&editedPoint)
-        return {curve: editedCurve, point:editedPoint};
+    if (editedCurve && editedPoint)
+      return { curve: editedCurve, point: editedPoint };
     return undefined;
   }
 
-  function handleMouseMove(event: MouseEvent) {
-    if (isDrawing) {
-      if (mode === "freehand") {
-        const newPoint: Point = [event.offsetX, event.offsetY];
-        push_point(newPoint);
-      } else if (mode === "edit") {
-        // Drag the closest point to the mouse with the mouse
-        if (editedPoint) {
-          curves[editedPoint.curve][editedPoint.point] = [event.offsetX, event.offsetY];
+  function smoothCurves(tolerance: number, state: State): State {
+    return produce(state, (draft) => {
+      draft.curves = draft.curves.map((curve) =>
+        douglasPeucker(curve, tolerance)
+      );
+    });
+  }
+
+  const reduce = (
+    action: Action,
+    mode: Mode,
+    state: State,
+    event: MouseEvent
+  ) => {
+    let newPoint: Point = [event.offsetX, event.offsetY];
+    if (mode === "freehand") {
+      if (action === "mousedown") {
+        state = startDrawing(push_point(newPoint, new_curve(state)));
+      } else if (action === "mouseup") {
+        if(state.drawing){
+            state = stopDrawing(finish_curve(state));
         }
-      } else if (mode === "lines") {
-        // Update the temporary line
-        tempLineStart = [event.offsetX, event.offsetY];
+      } else if (action === "mousemove") {
+        if(state.drawing)
+            state = push_point(newPoint, state);
+      }
+    } else if (mode === "lines") {
+      console.log(state)
+      if ( action === "mousedown" && !state.drawing)
+      {
+        state = startDrawing(push_point(newPoint, new_curve(state)));
+      }
+      else if (action === "mouseup") {
+        state = push_point(newPoint, state);
+      } else if (action === "mousemove") {
+        if (state.drawing) {
+          state = pop_point(state);
+          state = push_point(newPoint, state);
+        }
+      } else if (action === "mousedblclick") {
+        state = stopDrawing(finish_curve(state));
+      }
+    } else if (mode === "edit") {
+      if (action === "mousedown") {
+        editedPoint = GetEditedPoint(event);
+      } else if (action === "mouseup") {
+        editedPoint = undefined;
+      } else if (action === "mousemove") {
+        if (editedPoint) {
+          state = produce(state, (draft) => {
+            if (editedPoint != undefined)
+              draft.curves[editedPoint.curve][editedPoint.point] = [
+                event.offsetX,
+                event.offsetY,
+              ];
+          });
+        }
       }
     }
+
+    return state;
+  };
+
+  function handleMouseDown(event: MouseEvent) {
+    state = reduce("mousedown", mode, state, event);
+  }
+
+  function handleMouseMove(event: MouseEvent) {
+    state = reduce("mousemove", mode, state, event);
   }
 
   function handleMouseUp(event: MouseEvent) {
-    if (mode === "freehand") {
-      // End the current curve in freehand mode
-      isDrawing = false;
-      if (closeCurves && currentCurve.length > 1) {
-        push_point(currentCurve[0]);
-      }
-      currentCurve = [];
-    } else if (mode === "edit") {
-      editedPoint = undefined
-    }
+    state = reduce("mouseup", mode, state, event);
   }
 
-  function handleMouseOut() {
-    isDrawing = false;
-    if (mode === "freehand") {
-      // End the current curve in freehand mode
-      currentCurve = [];
-    } else if (mode === "lines") {
-      // Clear the temporary line
-      tempLineStart = null;
-    } else if (mode === "edit") {
-      editedPoint = undefined;
-    }
+  function handleMouseOut(event: any) {
+    state = reduce("mouseout", mode, state, event);
   }
 
-  function handleDoubleClick() {
-    if (mode === "lines" && currentCurve.length > 0) {
-      // End the current curve in line mode
-      isDrawing = false;
-      if (closeCurves && currentCurve.length > 1) {
-        push_point(currentCurve[0]);
-      }
-      currentCurve = [];
-      tempLineStart = null;
-    }
+  function handleDoubleClick(event: MouseEvent) {
+    state = reduce("mousedblclick", mode, state, event);
   }
 </script>
 
@@ -161,11 +212,7 @@
   on:dblclick={handleDoubleClick}
 >
   <!-- If tempLineStart exists then draw a line from the end of the current curve -->
-  {#if tempLineStart}
-    <Line start={tempLineStart} end={currentCurve[currentCurve.length - 1]} />
-  {/if}
-  <!-- Draw all polylines in curves -->
-  {#each curves as curve}
+  {#each state.curves as curve}
     <Polyline points={curve} />
   {/each}
 </Canvas>
@@ -183,8 +230,8 @@
     on:click={() => changeMode("edit")}
     class={mode === "edit" ? "active" : ""}>Edit</button
   >
-  <button on:click={() => clear()}>Reset</button>
-  <button on:click={() => smoothCurves(5)}>Smooth</button>
+  <button on:click={() => state = clear(state)}>Reset</button>
+  <button on:click={() => state = smoothCurves(5, state)}>Smooth</button>
   <label>
     Close curves
     <input type="checkbox" bind:checked={closeCurves} />
